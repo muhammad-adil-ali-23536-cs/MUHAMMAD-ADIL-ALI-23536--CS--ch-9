82e17e69dcef2cdffa93f7a6bd3a994fe7a5e5af
muhammad-adil-ali-23536-cs <adilwl289@gmail.com>
2024-08-27 19:07:32 +0500

    Add files via upload

---
+++ ch 9 qno 9.1.cpp
@@ -0,0 +1,13 @@
+// Write a program that displays a message "Programming makes life interesting " on screen using function.
+#include<iostream>
+using namespace std;
+void show(void);
+int main()
+{
+	show();
+	return 0;
+}
+void show()
+{
+	cout<<"Programming makes life interesting"<<endl;
+}
